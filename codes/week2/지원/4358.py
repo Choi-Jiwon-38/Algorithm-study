1af53efb37cb614a479ecf002b151d7104f12c39
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2026-01-20 22:54:38 +0900

    feat: 생태학, 최대 힙 코드 추가

---
+++ codes/week2/지원/4358.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+count = 0 
+dict = {}
+
+for _ in range(1000000):
+    ipt = input().rstrip()
+    if (ipt == ''): break
+
+    if ipt in dict:
+        dict[ipt] += 1
+    else:
+        dict[ipt] = 1
+
+    count += 1
+    
+
+keys = sorted(dict.keys())
+
+for key in keys:
+    print("%s %0.4f"%(key, dict[key] / count * 100))
